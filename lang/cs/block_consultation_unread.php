4af0bb73a96c71d2125bc7fc6adff4174bd69c42
Petr Skoda <info@skodak.org>
2010-08-01 19:08:39 +0200

    fixed lang pack quoting

--- lang/cs/block_consultation_unread.php
+++ lang/cs/block_consultation_unread.php
@@ -2,4 +2,4 @@
 
 $string['blockname'] = 'Nepřečtené dotazy';
 $string['nounread'] = 'Žádné';
-$string['unreaditem'] = '<a href=\"$a->link\">$a->name ($a->unread)</a>';
+$string['unreaditem'] = '<a href="$a->link">$a->name ($a->unread)</a>';
